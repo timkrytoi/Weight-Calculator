8e0676ba03314f683ad2ffdad89092197929fee9
timkrytoi <86091898+timkrytoi@users.noreply.github.com>
2022-01-21 11:49:52 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,97 @@
+print('weight programm')
+print('Example: height:202 weight: 97 ')
+n = int(input('Enter your height:'))
+n1 = int(input('Enter your weight:'))
+
+
+p = [147,146,145,144,148,149,150,143,142,141]
+n7 = [38,39,41,42,43,44,45,46,47]
+
+
+
+
+for i in p:
+    for r in n7:
+        if i == n and r == n1:
+            print('you have a normal weight')
+
+
+p1 = [151,152,153,154,155,156,157,158,159,160]
+k7 = [42,43,44,45,46,47,48,49,50]
+
+
+for g in p1:
+    for b in k7:
+        if g == n and b == n1:
+            print('you have a normal weight')
+
+
+
+
+p1 = [161,162,163,164,165,166,167,168,169,170]
+k7 = [42,43,44,45,46,47,48,49,50,51,52,53,54]
+
+
+for g in p1:
+    for b in k7:
+        if g == n and b == n1:
+            print('you have a normal weight')
+
+p1 = [171,172,173,174,175,176,177,178,179,180,181,182,]
+k7 = [54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70]
+
+
+for j in p1:
+    for b1 in k7:
+        if j == n and b1 == n1:
+            print('you have a normal weight')
+
+
+p6 = [183,184,185,186,187,188,189,190]
+k6 = [69,70,71,72,73,74,75,76,77,78,79,80,81,82,83]
+
+
+for n4 in p6:
+    for f4 in k6:
+        if n4 == n and f4 == n1:
+            print('you have a normal weight')
+
+
+
+p6 = [190, 191, 192, 193, 194, 195, 196, 197]
+k6 = [ 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,84,85,86,87,88,89,90,91,92,93,94,95,96,97]
+
+
+
+for l11 in p6:
+    for f12 in k6:
+        if l11 == n and f12 == n1:
+            print('you have a normal weight')
+
+
+p6 = [198,199,200,201]
+k6 = [74, 75, 76, 77, 78, 80, 81, 82, 83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100]
+
+
+
+for l1 in p6:
+    for f9 in k6:
+        if l1 == n and f9 == n1:
+            print('you have a normal weight')
+
+
+
+p6 = [202,203,204,205,206]
+k6 = [78, 79, 80, 81, 82, 83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106]
+
+
+
+for c in p6:
+    for f1 in k6:
+        if c == n and f1 == n1:
+            print('you have a normal weight')
+
+
+print("if  you don't see such an inscription as you have a normal weight,it means you have weight problems,or is it a bug in the program")
+
+
